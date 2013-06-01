30b0c943a07059cdf8bed7eb4f2426100ef5d108
Henrique Gubert <guberthenrique@hotmail.com>
2013-06-01 14:37:52 -0300

    Added operations 'drop_table' to migrations.

--- lib/cassandra_migrations/migration/table_operations.rb
+++ lib/cassandra_migrations/migration/table_operations.rb
@@ -11,6 +11,12 @@ module CassandraMigrations
     # - renaming tables
     module TableOperations
   
+      # Creates a new table in the keyspace
+      #
+      # options:
+      # - :primary_keys: single value or array (for compound primary keys). If
+      # not defined, some column must be chosen as primary key in the table definition. 
+      
       def create_table(table_name, options = {})
         create_table_helper = CreateTableHelper.new
         create_table_helper.define_primary_keys(options[:primary_keys]) if options[:primary_keys]
@@ -27,7 +33,14 @@ module CassandraMigrations
         
         execute create_cql
       end
+      
+      def drop_table(table_name)
+        announce_operation "drop_table(#{table_name})"
+        drop_cql =  "DROP TABLE #{table_name}"
+        announce_suboperation drop_cql
         
+        execute drop_cql
+      end
     end
   end
 end
