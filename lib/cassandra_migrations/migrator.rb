443caa8d3f2f6c8c38e9ec111d5c0b6766473fb1
laythra <laythra@gmail.com>
2019-11-21 15:55:28 +0200

    fix schema path

--- lib/cassandra_migrations/migrator.rb
+++ lib/cassandra_migrations/migrator.rb
@@ -16,7 +16,7 @@ module CassandraMigrations
         new_migrations.each { |migration| up(migration) }
       end
 
-      system("cqlsh -e 'DESC KEYSPACE cards_#{Rails.env}' > '~/workspace/jawaker/web/db/cassandra_schema.cql'")
+      system("cqlsh -e 'DESC KEYSPACE cards_#{Rails.env}' > './db/cassandra_schema.cql'")
       new_migrations.size
     end
 
